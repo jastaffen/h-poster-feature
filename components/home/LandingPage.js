9aff08e55eb17ab8c2d886f82d12daad56cd607f
jastaffen <jack.staffen@gmail.com>
2020-05-01 00:52:33 -0400

    add tabbar to landing page

--- components/home/LandingPage.js
+++ components/home/LandingPage.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const LandingPage = () => (
-    <Text>Hi</Text>
-)
+
+import Canvas from '../canvas/Canvas';
+import ViewPosters from '../posters/ViewPosters';
+
+const Tab = createBottomTabNavigator();
+
+const tabBarOptions = {
+    inactiveBackgroundColor: 'white',
+    activeBackgroundColor: 'black',
+    tabStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        borderTopWidth: 1,
+        borderTopColor: 'white'
+    },
+    labelStyle: {
+        fontSize: 15,
+        fontFamily: 'Helvetica'
+    },
+    activeTintColor: 'white'
+ }
+
+const LandingPage = () => {
+    return(
+        <Tab.Navigator tabBarOptions={ tabBarOptions } >
+            <Tab.Screen name="CREATE" component={ Canvas } />
+            <Tab.Screen name="VIEW" component={ ViewPosters } />
+        </Tab.Navigator>
+    )
+}
 
 export default LandingPage;
